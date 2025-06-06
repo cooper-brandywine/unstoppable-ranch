199e1d20ea6db1b430f87f0cc7e911d6ddafd13d
cooper-brandywine <106340418+cooper-brandywine@users.noreply.github.com>
2025-06-06 09:32:54 -0700

    add shop button

--- js/header.js
+++ js/header.js
@@ -19,6 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
         <span></span>
       </div>
       
+      <div class="shop-now">
+        <a class="pop-button" href="/merch">Shop</a>
+      </div>
       <div class="donate-now">
         <a class="pop-button" href="https://www.paypal.com/donate/?hosted_button_id=F46DZZ5KCQD72" target="_blank">Donate</a>
       </div>
